69bc033d8d89d073cc493b29e0a36d7e379af095
Luis Henrique <lhzimmermann@gmail.com>
2020-01-24 17:15:30 -0300

    Commit - Aula 3
    Partial Application + Composição de funções

--- project/public/app/nota/service.js
+++ project/public/app/nota/service.js
@@ -1,14 +1,20 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, pipe } from '../utils/operators.js';
 
 const API = `http://localhost:3000/notas`;
+
+// não existe mais a função `sumItems`. Ela foi substituída por três funções
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
+const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 // flatMap = retornará para o próximo then uma lista de itens
 // filter = retornará para o próximo then uma lista de itens filtrada
 // reduce = retornará para o próximo then o total 
-const sumItems = code => notas => 
-    notas.$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0)
+// const sumItems = code => notas => 
+//     notas.$flatMap(nota => nota.itens)
+//     .filter(item => item.codigo == code)
+//     .reduce((total, item) => total + item.valor, 0)
 
 export const notasService = {
     listAll() {
@@ -24,6 +30,13 @@ export const notasService = {
     },
 
     sumItems(code) {
-      return this.listAll().then(sumItems(code));
+      // usando pipe e alterando a ordem dos parâmetros
+      const sumItems = pipe(
+        getItemsFromNotas,
+        partialize(filterItemsByCode, code), 
+        sumItemsValue, 
+      );
+
+      return this.listAll().then(sumItems);
     }
 };
