81495fd016461235caa2435a3a9da545767197eb
Luis Henrique <lhzimmermann@gmail.com>
2020-01-27 17:33:47 -0300

    Commit - Aula 5
    Promise.race, timeout, delay e retry com promises

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,18 +1,17 @@
-import {log} from './utils/promise-helpers.js'
+import {log, timeoutPromise, retry} from './utils/promise-helpers.js'
 import'./utils/array-helpers.js'
 import { notasService as service } from './nota/service.js'; 
-import { takeUntil, debounceTime, pipe } from './utils/operators.js'
+import { takeUntil, debounceTime, pipe, partialize } from './utils/operators.js'
 
 const operations = pipe(
-  partialize(debounceTime, 500), 
-  partialize(takeUntil, 3)
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500) 
 );
 
 const action = operations(() => 
-  service
-  .sumItems('2143')
-  .then(console.log)
-  .catch(console.log)
+   retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+   .then(console.log)
+   .catch(console.log)
 );
 
 document
